bb650ae2580a4111e024450d9681a7070e4817ca
Thais Tamaio <77994638+ThaisTamaio@users.noreply.github.com>
2023-11-23 16:19:46 -0500

    wiiii

--- src/aerolinea/aerolinea.service.spec.ts
+++ src/aerolinea/aerolinea.service.spec.ts
@@ -4,23 +4,114 @@ import { Repository } from 'typeorm';
 import { TypeOrmTestingConfig } from '../shared/testing-utils/typeorm-testing-config';
 import { AerolineaEntity } from './aerolinea.entity';
 import { AerolineaService } from './aerolinea.service';
+import { faker } from '@faker-js/faker';
+import { BadRequestException } from '@nestjs/common';
 
 describe('AerolineaService', () => {
- let service: AerolineaService;
- let repository: Repository<AerolineaEntity>;
+  let service: AerolineaService;
+  let repository: Repository<AerolineaEntity>;
+  let aerolineaList: AerolineaEntity[];
+  let deleteSpy: { calledWithId: string | null };
 
- beforeEach(async () => {
-   const module: TestingModule = await Test.createTestingModule({
-     imports: [...TypeOrmTestingConfig()],
-     providers: [AerolineaService],
-   }).compile();
+  beforeEach(async () => {
+    aerolineaList = Array.from({ length: 5 }).map(() => ({
+      id: faker.datatype.uuid(),
+      nombre: faker.company.companyName(),
+      descripcion: faker.lorem.sentence(),
+      fechaFundacion: faker.date.past(),
+      paginaWeb: faker.internet.url(),
+      aeropuertos: [],
+    }));
 
-   service = module.get<AerolineaService>(AerolineaService);
-   repository = module.get<Repository<AerolineaEntity>>(getRepositoryToken(AerolineaEntity));
- });
-  
- it('should be defined', () => {
-   expect(service).toBeDefined();
- });
+    deleteSpy = { calledWithId: null };
 
+    const mockAerolineaRepository = {
+      find: jest.fn().mockResolvedValue(aerolineaList),
+      findOne: jest.fn().mockImplementation((options) => {
+        const id = options.where.id;
+        return Promise.resolve(aerolineaList.find(a => a.id === id));
+      }),
+      save: jest.fn().mockImplementation((aero) => Promise.resolve({ id: faker.datatype.uuid(), ...aero })),
+      delete: jest.fn().mockImplementation((id: string) => {
+        deleteSpy.calledWithId = id;
+        return Promise.resolve({ affected: 1 });
+      }),
+    };
+
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [...TypeOrmTestingConfig()],
+      providers: [
+        AerolineaService,
+        {
+          provide: getRepositoryToken(AerolineaEntity),
+          useValue: mockAerolineaRepository,
+        },
+      ],
+    }).compile();
+
+    service = module.get<AerolineaService>(AerolineaService);
+    repository = module.get<Repository<AerolineaEntity>>(getRepositoryToken(AerolineaEntity));
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('findAll should return all aerolineas', async () => {
+    const result = await service.findAll();
+    expect(result).toEqual(aerolineaList);
+  });
+
+  it('findOne should return an aerolinea by ID', async () => {
+    const aerolinea = aerolineaList[0];
+    const result = await service.findOne(aerolinea.id);
+    expect(result).toEqual(aerolinea);
+  });
+
+  it('create should fail if the fundation date is in the future', async () => {
+    const newAerolinea = {
+      ...aerolineaList[0],
+      fechaFundacion: new Date(new Date().getTime() + 86400000), // Fecha en el futuro
+    };
+    try {
+      await service.create(newAerolinea as AerolineaEntity);
+    } catch (e) {
+      expect(e).toBeInstanceOf(BadRequestException);
+    }
+  });
+
+  it('create should succeed with a past fundation date', async () => {
+    const newAerolinea = {
+      ...aerolineaList[0],
+      fechaFundacion: new Date(new Date().getTime() - 86400000), // Fecha en el pasado
+    };
+    const result = await service.create(newAerolinea as AerolineaEntity);
+    expect(result).toEqual(newAerolinea);
+  });
+
+  it('update should fail if the aerolinea is not found', async () => {
+    try {
+      await service.update(faker.datatype.uuid(), {});
+    } catch (e) {
+      expect(e).toBeInstanceOf(BadRequestException);
+    }
+  });
+
+  it('update should fail if the fundation date is in the future', async () => {
+    const aerolineaToUpdate = {
+      ...aerolineaList[0],
+      fechaFundacion: new Date(new Date().getTime() + 86400000), // Fecha en el futuro
+    };
+    try {
+      await service.update(aerolineaList[0].id, aerolineaToUpdate as Partial<AerolineaEntity>);
+    } catch (e) {
+      expect(e).toBeInstanceOf(BadRequestException);
+    }
+  });
+
+  it('delete should remove an aerolinea', async () => {
+    const aerolineaIdToDelete = aerolineaList[0].id;
+    await service.delete(aerolineaIdToDelete);
+    expect(deleteSpy.calledWithId).toEqual(aerolineaIdToDelete);
+  });
 });
--- src/aeropuerto/aeropuerto.service.spec.ts
+++ src/aeropuerto/aeropuerto.service.spec.ts
@@ -5,27 +5,103 @@ import { AeropuertoEntity } from './aeropuerto.entity';
 import { AeropuertoService } from './aeropuerto.service';
 import { TypeOrmTestingConfig } from '../shared/testing-utils/typeorm-testing-config';
 import { faker } from '@faker-js/faker';
+import { BadRequestException } from '@nestjs/common';
 
 describe('AeropuertoService', () => {
   let service: AeropuertoService;
   let aeropuertoRepository: Repository<AeropuertoEntity>;
-  let aeropuertoList: AeropuertoEntity[] = [];
+  let aeropuertoList: AeropuertoEntity[];
+  let deleteSpy: { calledWithId: string | null };
 
   beforeEach(async () => {
+    aeropuertoList = Array.from({ length: 5 }).map(() => ({
+      id: faker.datatype.uuid(),
+      nombre: faker.address.cityName(),
+      codigo: faker.random.alpha({ count: 3 }).toUpperCase(),
+      pais: faker.address.country(),
+      ciudad: faker.address.city(),
+      aerolineas: [],
+    }));
+
+    deleteSpy = { calledWithId: null };
+
     const module: TestingModule = await Test.createTestingModule({
       imports: [...TypeOrmTestingConfig()],
-      providers: [AeropuertoService],
+      providers: [
+        AeropuertoService,
+        {
+          provide: getRepositoryToken(AeropuertoEntity),
+          useValue: {
+            find: jest.fn().mockResolvedValue(aeropuertoList),
+            findOne: jest.fn().mockImplementation((options) => {
+              const id = options.where.id;
+              return Promise.resolve(aeropuertoList.find(a => a.id === id));
+            }),
+            save: jest.fn().mockImplementation((aero) => Promise.resolve({ id: faker.datatype.uuid(), ...aero })),
+            delete: jest.fn().mockImplementation((id: string) => {
+              deleteSpy.calledWithId = id;
+              return Promise.resolve({ affected: 1 });
+            }),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<AeropuertoService>(AeropuertoService);
-    aeropuertoRepository = module.get<Repository<AeropuertoEntity>>(
-      getRepositoryToken(AeropuertoEntity),
-    );
-
+    aeropuertoRepository = module.get<Repository<AeropuertoEntity>>(getRepositoryToken(AeropuertoEntity));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 
+  it('findAll should return all airports', async () => {
+    const result = await service.findAll();
+    expect(result).toEqual(aeropuertoList);
+  });
+
+  it('findOne should return an airport by ID', async () => {
+    const aeropuerto = aeropuertoList[0];
+    const result = await service.findOne(aeropuerto.id);
+    expect(result).toEqual(aeropuerto);
+  });
+
+  it('create should fail if the airport code has more than 3 characters', async () => {
+    try {
+      await service.create({ ...aeropuertoList[0], codigo: 'ABCD' });
+    } catch (e) {
+      expect(e).toBeInstanceOf(BadRequestException);
+    }
+  });
+
+  it('create should succeed with a 3-character airport code', async () => {
+    const newAeropuerto = {
+      ...aeropuertoList[0],
+      id: faker.datatype.uuid(),
+    };
+    const result = await service.create(newAeropuerto);
+    expect(result).toEqual(newAeropuerto);
+  });
+
+  it('update should fail if the airport code has more than 3 characters', async () => {
+    try {
+      await service.update(aeropuertoList[0].id, { codigo: 'ABCD' });
+    } catch (e) {
+      expect(e).toBeInstanceOf(BadRequestException);
+    }
+  });
+
+  it('update should succeed with a 3-character airport code', async () => {
+    const updatedAeropuerto = { ...aeropuertoList[0], codigo: 'ABC' };
+    const result = await service.update(aeropuertoList[0].id, updatedAeropuerto);
+    expect(result.codigo).toEqual('ABC');
+  });
+
+  it('delete should remove an airport', async () => {
+    const aeropuertoIdToDelete = aeropuertoList[0].id;
+    await service.delete(aeropuertoIdToDelete);
+
+    expect(deleteSpy.calledWithId).toEqual(aeropuertoIdToDelete);
+  });
+  
 });
